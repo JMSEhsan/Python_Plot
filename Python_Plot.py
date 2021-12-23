f202a42d20399559cb39fdcf234632cf95cf39dc
Ehsan <JMSEhsan@gmail.com>
2021-12-23 00:23:03 -0700

    commit

--- Python_Plot.py
+++ Python_Plot.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 from math import sin
 A =[]
 j = 0
+k = 0
 while j < 100:
     j+=1
-    A.insert(j-1, j)
-SA = [sin(i) for i in A]
+    k+=0.3
+    A.insert(j-1, k)
+SA = [10*sin(i) for i in A]
 plt.plot(SA)
 plt.plot(range(100))
 plt.show()
